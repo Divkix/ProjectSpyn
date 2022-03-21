e3082db386eb6a479ec2fa56eef32d93b9deeed9
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-03-21 10:56:25 -0700

    Some fixes to color sensor thing

--- ProjectSpyn.m
+++ ProjectSpyn.m
@@ -2,9 +2,6 @@
 brick.SetColorMode(2, 2);
 
 % define some variables
-% stopDistance = 13.5;  % stopDistance is the distance after which the robot should not move
-% canTurnRight = false;  % canTurnRight == true means than robot can turn right, otherwise it cannot
-% canTurnLeft = false;  % canTurnLeft == true means than robot can turn left, otherwise it cannot
 robotSpeed = 50;  % the speed at which robot will move around the maze
 
 while true
@@ -18,7 +15,7 @@ while true
     moveForward();
     
     % jit = legoev3
-    % mycolorsensor = colorsensor(git)
+    % mycolorsensor = colorsensor(jit)
     % color = readColor(mycolorsensor)
     if color == 4
         % move forward
@@ -39,51 +36,16 @@ while true
     end
 
     
-    % if distance == stopDisatnce, then stop the robot and decide where to
-    % turn
-    if distance == 13.5
+    % if distance <= stopDisatnce, then stop the robot and decide where to turn
+    if distance <= 13.5
         stop();
         turnRight();
+        pause(1);
         turnLeft();
+        pause(1);
         turnLeft();
+        pause(1);
         turnRight();
+        pause(1);
     end
-
-    % if robot can turn and move to right, then turn right
-    % elseif it cannot move right but can turn and move to left, turn left
-    % else turn the robotBackward
-    % if canTurnRight == true
-    %     turnRight();
-    %     moveForward();
-    % elseif canTurnLeft == true
-    %     turnLeft();
-    %     moveForward();
-    % else
-    %     turnBackward()
-    % end
-
 end
-
-
-% % function to reset the direction of robot to zero
-% function resetRobot()
-%     global brick
-%     % put in a loop to make sure robot gets stabalized
-%     while true
-%         % get the angle from sensor
-%         angle = brick.GyroAngle(1);
-%         if angle == 0 || angle == 360
-%             % if angle is 0 or 360, break out of loop
-%             break;
-%         elseif angle > 0 && angle < 180
-%             % turn left if angle is greater than 0 but smaller than 180
-%             turnLeft();
-%         elseif angle == 180
-%             % turn right if gyro angle is 180
-%             turnRight();
-%         elseif angle > 180 && angle < 360
-%             % turn right if angle is greater than 180 but smaller than 360
-%             turnRight();
-%         end
-%     end
-% end
