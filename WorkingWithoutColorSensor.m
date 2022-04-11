88d05e4a64c68f06eae2ea3338e10f5b2b2190dc
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-04-11 10:42:55 -0700

    Working code without color sensor
    Co-authored-by: JohnFazi <JohnFazi@users.noreply.github.com>
    Co-authored-by: noraelliott <noraelliott@users.noreply.github.com>

--- WorkingWithoutColorSensor.m
+++ WorkingWithoutColorSensor.m
@@ -1,4 +1,4 @@
-%brick.SetColorMode(2, 2);
+brick.SetColorMode(2, 2);
 
 while true
 
@@ -16,17 +16,19 @@ while true
             % display the distance
             disp("Distance Right: " + distanceRight);
 
-            if distanceRight > 12 && distanceRight < 19
+            if distanceRight > 14 && distanceRight < 21
                 % if distance is more than 12 cm and less than 19, move forward
                 disp("Moving forward");
                 brick.MoveMotor('C', -46);
                 brick.MoveMotor('A', -50);
-            elseif distanceRight < 12
+            elseif distanceRight < 14
                 % if distance is less than 12 cm, turn left 100 degrees
+                pause(.08);
                 disp("Turning left");
                   brick.MoveMotor('C', 30);
                   brick.MoveMotor('A', -30);
-            elseif distanceRight > 19
+            elseif distanceRight > 21
+                pause(.08);
                 % if distance is more than 19 cm, turn right 100 degrees
                 disp("Turning right");
                 brick.MoveMotor('C', -30);
@@ -40,5 +42,8 @@ while true
         % turn left 90 degrees
         brick.MoveMotor('C', 30);
         brick.MoveMotor('A', -30);
-        pause(1.5);
+        pause(1);
     end
+
+   
+end
